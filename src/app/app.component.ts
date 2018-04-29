77f844f29cb3999c58d5dedf592777146282ca0a
Dante <relaxurbano@me.com>
2018-04-30 00:22:31 +0200

    pl
    pl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare let FB: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title:"AS";
-  
+  fbAsyncInit()  {
+    FB.init({
+      appId      : '2055364314701838',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.12'
+    });
+    FB.getLoginStatus(function(response) {
+      
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+  loading = ()=>{
+    var js, fjs = document.getElementsByTagName('script')[0];
+     if (document.getElementById('facebook-jssdk')) {return;}
+     js = document.createElement('script'); 
+     js.id = 'facebook-jssdk';
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+  }
+  statusChangeCallback(response){
+     if(response.status === 'connected'){
+       console.log("logeado y autenticado correctamente");
+     }else{
+       console.log("not autenticado")
+     }
+   }
+   checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      console.log(response.status);
+    })
+  }
 }
