8f5896d0dba282ab615cd7b4d68628e6c52691e9
Dante <relaxurbano@me.com>
2018-05-03 15:29:48 +0200

    asd
    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { AuthService } from "angularx-social-login";
+import { AuthService,SocialUser } from "angularx-social-login";
 import { FacebookLoginProvider, GoogleLoginProvider, LinkedInLoginProvider } from "angularx-social-login";
 
 @Component({
@@ -8,27 +8,25 @@ import { FacebookLoginProvider, GoogleLoginProvider, LinkedInLoginProvider } fro
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  user:SocialUser;
   constructor(private authService: AuthService) { }
 
-  signInWithGoogle(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-
+  
   signInWithFB(): void {
     this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
     console.log(FacebookLoginProvider.PROVIDER_ID)
     console.log(this.authService.signIn(FacebookLoginProvider.PROVIDER_ID));
   }
   
-  signInWithLinkedIn(): void {
-    this.authService.signIn(LinkedInLoginProvider.PROVIDER_ID);
-  }  
-
+  
   signOut(): void {
     this.authService.signOut();
   } 
   ngOnInit(){
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user)
+    });
 
   }
 }
